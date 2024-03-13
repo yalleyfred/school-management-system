6a415a60a76c69f51ffba904f9990fa595b75a38
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-13 11:04:55 +0000

    refactor: create and import 'top-performer-table' component into 'dashnoard'

--- apps/sms-client/src/app/pages/dasboard/dasboard.component.ts
+++ apps/sms-client/src/app/pages/dasboard/dasboard.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardTileComponent } from '../../components/dashboard-tile/dashboard-tile.component';
 import { CalendarComponent } from '../../components/calendar/calendar.component';
 import { EventTileComponent } from '../../components/event-tile/event-tile.component';
+import { TopPerfomerTableComponent } from '../../components/top-perfomer-table/top-perfomer-table.component';
 
 @Component({
   selector: 'sms-dasboard',
@@ -11,7 +12,8 @@ import { EventTileComponent } from '../../components/event-tile/event-tile.compo
     CommonModule,
     DashboardTileComponent,
     CalendarComponent,
-    EventTileComponent
+    EventTileComponent,
+    TopPerfomerTableComponent
   ],
   templateUrl: './dasboard.component.html',
   styleUrl: './dasboard.component.css',
