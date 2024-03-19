36424fa10a1060c80790d6ba1b6edd4f9cff183d
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-19 09:37:40 +0000

    feat: create column-chart component

---
+++ apps/sms-client/src/app/components/column-chart/column-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sms-column-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './column-chart.component.html',
+  styleUrl: './column-chart.component.css',
+})
+export class ColumnChartComponent {}
