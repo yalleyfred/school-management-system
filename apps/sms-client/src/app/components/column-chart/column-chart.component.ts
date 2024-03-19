038c926e0cf964dee39ccaba68cd25e44c94643e
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-19 10:25:06 +0000

    refactor: render column-chart

--- apps/sms-client/src/app/components/column-chart/column-chart.component.ts
+++ apps/sms-client/src/app/components/column-chart/column-chart.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'sms-column-chart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxChartsModule],
   templateUrl: './column-chart.component.html',
   styleUrl: './column-chart.component.css',
 })
