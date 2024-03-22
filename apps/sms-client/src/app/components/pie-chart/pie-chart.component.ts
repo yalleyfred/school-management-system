997c7c4026b15e81cdbdd440d9d989f66df12a96
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-22 13:07:35 +0000

    refactor: replace 'ngx-charts' with custom chart

--- apps/sms-client/src/app/components/pie-chart/pie-chart.component.ts
+++ apps/sms-client/src/app/components/pie-chart/pie-chart.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+// import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'sms-pie-chart',
   standalone: true,
-  imports: [CommonModule, NgxChartsModule],
+  imports: [CommonModule],
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.css',
 })
-export class PieChartComponent {}
+export class PieChartComponent {
+  isGreaterThanHalf = true;
+
+  total= 500
+  teachers= 200
+  students=300
+}
