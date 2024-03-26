eb290244241b86db1135b6f3b00e93bfc1626501
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-26 15:38:57 +0000

    refactor: create 'detail' component

--- apps/sms-client/src/app/pages/student/student.component.ts
+++ apps/sms-client/src/app/pages/student/student.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataListComponent } from '../../components/data-list/data-list.component';
+import { DetailComponent } from '../../components/detail/detail.component';
 
 @Component({
   selector: 'sms-student',
   standalone: true,
-  imports: [CommonModule, DataListComponent],
+  imports: [CommonModule, DataListComponent, DetailComponent],
   templateUrl: './student.component.html',
   styleUrl: './student.component.css',
 })
