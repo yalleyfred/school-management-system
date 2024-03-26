78ee3aa066dd0ca76f28eb1b307e92fd1bef437d
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-26 10:27:49 +0000

    refactor: create 'data-list' component

---
+++ apps/sms-client/src/app/components/data-list/data-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sms-data-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-list.component.html',
+  styleUrl: './data-list.component.css',
+})
+export class DataListComponent {}
