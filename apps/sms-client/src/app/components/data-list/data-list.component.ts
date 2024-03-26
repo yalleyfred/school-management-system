9d7544dd949ad6c090ca368a039074581b3dcba1
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-26 11:22:25 +0000

    refactor: create and import data-item component into data-list

--- apps/sms-client/src/app/components/data-list/data-list.component.ts
+++ apps/sms-client/src/app/components/data-list/data-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataItemComponent } from '../data-item/data-item.component';
 
 @Component({
   selector: 'sms-data-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DataItemComponent],
   templateUrl: './data-list.component.html',
   styleUrl: './data-list.component.css',
 })
--- apps/sms-client/src/app/pages/student/student.component.ts
+++ apps/sms-client/src/app/pages/student/student.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataListComponent } from '../../components/data-list/data-list.component';
 
 @Component({
-  selector: 'school-management-system-student',
+  selector: 'sms-student',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DataListComponent],
   templateUrl: './student.component.html',
   styleUrl: './student.component.css',
 })
