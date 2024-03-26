2679a61d915162dc3f20af07b2b2d7a1efbacfd8
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-03-26 13:41:43 +0000

    refactor: move 'search bar' to search component and import

--- apps/sms-client/src/app/components/navbar/navbar.component.ts
+++ apps/sms-client/src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { getCurrentPage } from '../../utils/general';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'sms-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  private router = inject(Router)
+  private router = inject(Router);
 
-  public getPageName(){
-    return this.router.url.split('/').pop()
-  }  
+  public getPageName() {
+    return getCurrentPage(this.router.url);
+  }
 }
--- apps/sms-client/src/app/components/search/search.component.ts
+++ apps/sms-client/src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
-export class SearchComponent {}
+export class SearchComponent {
+  @Input() public placeholder=''
+}
