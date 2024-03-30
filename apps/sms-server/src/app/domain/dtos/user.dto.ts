b02a8f4216dc6e4b462df2f7afc280cdd2240cc7
Fredrick Yalley <yalleyfred@gmail.com>
2024-03-30 01:33:27 +0000

    feat: add user dto

---
+++ apps/sms-server/src/app/domain/dtos/user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+}
