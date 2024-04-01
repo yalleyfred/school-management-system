85486538494c575421d16493d87b060f2aa4920d
Fredrick Yalley <yalleyfred@gmail.com>
2024-04-01 19:12:19 +0000

    feat: configure config and typeorm in app

--- apps/sms-server/src/app/app.module.ts
+++ apps/sms-server/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './domain/entities/user.entity';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
-    port: 5433,
-    username: 'postgres',
-    password: 'user',
-    database: 'sms',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [UserEntity], 
     synchronize: true, // You can set this to false in production
   }), UsersModule, AuthModule],
