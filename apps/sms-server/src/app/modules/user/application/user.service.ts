8c4e6f1f4952a7e5f6ccc18ae97ceeb61968e9e8
Fredrick Yalley <yalleyfred@gmail.com>
2024-04-06 12:04:48 +0000

    feat: add update user

--- apps/sms-server/src/app/modules/user/application/user.service.ts
+++ apps/sms-server/src/app/modules/user/application/user.service.ts
@@ -1,15 +1,19 @@
 
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository, UpdateResult } from 'typeorm';
 import { UserEntity } from '../../../domain/entities/user.entity';
 import { NewUserDTO } from '../../../domain/dtos/auth.dto';
+import { User } from '../../../domain/model/user.model';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectDataSource()
+    private readonly dataSource: DataSource,
   ) {}
 
   async findAll(): Promise<UserEntity[]> {
@@ -24,7 +28,64 @@ export class UserService {
     return this.userRepository.save(user);
   }
 
-  async remove(id: number): Promise<void> {
-    await this.userRepository.delete(id);
+  async remove(userId: string): Promise<void> {
+    await this.userRepository.delete(userId);
   }
+
+  public async updateUser(userId: string, userData: Partial<User>): Promise<UpdateResult> {
+    return this.dataSource
+      .createQueryBuilder()
+      .update(UserEntity)
+      .set({
+        ...userData,
+      })
+      .where('id = :userId and hash_rt IS NOT NULL', { userId })
+      .execute();
+  }
+
+  public async createHash(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+
+  public async compareHashes(password, userHash: string): Promise<boolean> {
+    return bcrypt.compare(password, userHash);
+  }
+
+  public async findUserByEmail(email: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    return user;
+  }
+
+  public async removeUserByEmail(email: string): Promise<number> {
+    const result = await this.userRepository.delete({ email });
+
+    return result.affected;
+  }
+
+  public async findUserById(id: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    return user;
+  }
+
+  public async getUserAsStringById(id: string): Promise<string> {
+    const fullUser = await this.findUserById(id);
+
+    return `${fullUser.firstName} ${fullUser.lastName}`;
+  }
+
+  public async updateRtHash(userId: string, rt: string): Promise<void> {
+    const hash = await bcrypt.hash(rt, 10);
+
+    await this.dataSource
+      .createQueryBuilder()
+      .update(UserEntity)
+      .set({
+        hastRt: hash,
+      })
+      .where('id = :userId', { userId })
+      .execute();
+  }
+
 }
